4f325eed6f525a2a9567cbb238b900f1d1fb02e5
Itzsplicez <155481146+Itzsplicez@users.noreply.github.com>
2025-12-26 14:19:03 -0800

    Update speed.lua

--- speed.lua
+++ speed.lua
@@ -3,7 +3,7 @@ elseif cmd:sub(1,6) == "/speed" then
     local speedNum = tonumber(arg)
     if speedNum and speedNum >= 1 and speedNum <= 100 then
         if player.Character and player.Character:FindFirstChild("Humanoid") then
-            local newSpeed = 20 + ((speedNum - 1) * (100 - 20)/99)
+            local newSpeed = speedNum -- directly use the number for WalkSpeed
             player.Character.Humanoid.WalkSpeed = newSpeed
         end
         printToTerminal("Speed set to "..speedNum)
