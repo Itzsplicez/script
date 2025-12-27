01e9f9b543e18d3a49b8c2c64fd264909cf69f9e
Itzsplicez <155481146+Itzsplicez@users.noreply.github.com>
2025-12-27 10:46:24 -0800

    Create loopspeed.lua

---
+++ loopspeed.lua
@@ -0,0 +1,40 @@
+-- Loopspeed module
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+local loopConnection
+local LoopSpeedModule = {}
+
+function LoopSpeedModule.Start(speed)
+    local speedNum = tonumber(speed)
+    if not speedNum or speedNum < 1 or speedNum > 100 then
+        return false, "Invalid speed! Use 1-100"
+    end
+
+    -- Stop previous loop if it exists
+    if loopConnection then
+        loopConnection:Disconnect()
+        loopConnection = nil
+    end
+
+    -- Start looping
+    loopConnection = RunService.Heartbeat:Connect(function()
+        if player.Character and player.Character:FindFirstChild("Humanoid") then
+            player.Character.Humanoid.WalkSpeed = speedNum
+        end
+    end)
+
+    print("Loopspeed started at "..speedNum)
+    return true
+end
+
+function LoopSpeedModule.Stop()
+    if loopConnection then
+        loopConnection:Disconnect()
+        loopConnection = nil
+        print("Loopspeed stopped")
+    end
+end
+
+return LoopSpeedModule
